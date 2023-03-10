565776509bf225c17ba04ad931a75627f3f46212
oviliac <oviliachyntya@gmail.com>
2023-03-11 06:19:01 +0700

    login locator

--- locator.py
+++ locator.py
@@ -11,3 +11,19 @@ class RegisterPage:
     error_summary = "validation-summary-errors"
     error_password = ".field-validation-error[data-valmsg-for='Password']"
     error_confirmpassword = ".field-validation-error[data-valmsg-for='ConfirmPassword']"
+
+class LoginPage:
+    email = "Email"
+    password = "Password"
+    rememberme = "RememberMe"
+    login_button = ".button-1.login-button[type=submit]"
+    profile_info = ".header-links > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)"
+    forgot_password = ".forgot-password > a:nth-child(1)"
+    error_summary = ".validation-summary-errors > span:nth-child(1)"
+    error_summary_detail = ".validation-summary-errors > ul:nth-child(2) > li:nth-child(1)"
+
+class RecoveryPage:
+    header = ".page-title > h1:nth-child(1)"
+    email = "Email"
+    recover_button = ".password-recovery-button[type=submit]"
+    result = ".result"
--- login.py
+++ login.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
 from data import DataLogin
+from locator import LoginPage, RecoveryPage
 
 class TestLogin(unittest.TestCase):
 
@@ -20,112 +21,112 @@ class TestLogin(unittest.TestCase):
 
     def test_01_success_login(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys(DataLogin.email)
-        driver.find_element(By.ID, "Password").send_keys(DataLogin.password)
-        driver.find_element(By.ID, "RememberMe").click()
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        self.assertEqual(driver.current_url, "https://demowebshop.tricentis.com/")
-        data = driver.find_element(By.CSS_SELECTOR, ".header-links > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)").text
-        self.assertEqual("oviliach123@gmail.com", data)
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys(DataLogin.email)
+        driver.find_element(By.ID, LoginPage.password).send_keys(DataLogin.password)
+        driver.find_element(By.ID, LoginPage.rememberme).click()
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        self.assertEqual(driver.current_url, DataLogin.baseURL)
+        data = driver.find_element(By.CSS_SELECTOR, LoginPage.profile_info).text
+        self.assertEqual(DataLogin.email, data)
 
     def test_02_invalid_username_and_password(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys("awqqhj@xsasai.com")
-        driver.find_element(By.ID, "Password").send_keys("1234567a")
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        error1 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > span:nth-child(1)").text
-        error2 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > ul:nth-child(2) > li:nth-child(1)").text
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys("awqqhj@xsasai.com")
+        driver.find_element(By.ID, LoginPage.password).send_keys("1234567a")
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        error1 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary).text
+        error2 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary_detail).text
         self.assertEqual("Login was unsuccessful. Please correct the errors and try again.", error1)
         self.assertEqual("No customer account found", error2)
 
     def test_03_invalid_username_and_valid_password(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys("awqqhj@xsasai.com")
-        driver.find_element(By.ID, "Password").send_keys(DataLogin.password)
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        error1 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > span:nth-child(1)").text
-        error2 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > ul:nth-child(2) > li:nth-child(1)").text
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys("awqqhj@xsasai.com")
+        driver.find_element(By.ID, LoginPage.password).send_keys(DataLogin.password)
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        error1 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary).text
+        error2 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary_detail).text
         self.assertEqual("Login was unsuccessful. Please correct the errors and try again.", error1)
         self.assertEqual("No customer account found", error2)
         
     def test_04_valid_username_and_invalid_password(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys(DataLogin.email)
-        driver.find_element(By.ID, "Password").send_keys("12345")
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        error1 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > span:nth-child(1)").text
-        error2 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > ul:nth-child(2) > li:nth-child(1)").text
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys(DataLogin.email)
+        driver.find_element(By.ID, LoginPage.password).send_keys("12345")
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        error1 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary).text
+        error2 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary_detail).text
         self.assertEqual("Login was unsuccessful. Please correct the errors and try again.", error1)
         self.assertEqual("The credentials provided are incorrect", error2)
 
     def test_05_empty_email(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys("")
-        driver.find_element(By.ID, "Password").send_keys(DataLogin.password)
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        error1 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > span:nth-child(1)").text
-        error2 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > ul:nth-child(2) > li:nth-child(1)").text
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys("")
+        driver.find_element(By.ID, LoginPage.password).send_keys(DataLogin.password)
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        error1 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary).text
+        error2 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary_detail).text
         self.assertEqual("Login was unsuccessful. Please correct the errors and try again.", error1)
         self.assertEqual("No customer account found", error2)
 
     def test_06_empty_password(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys(DataLogin.email)
-        driver.find_element(By.ID, "Password").send_keys("")
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        error1 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > span:nth-child(1)").text
-        error2 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > ul:nth-child(2) > li:nth-child(1)").text
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys(DataLogin.email)
+        driver.find_element(By.ID, LoginPage.password).send_keys("")
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        error1 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary).text
+        error2 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary_detail).text
         self.assertEqual("Login was unsuccessful. Please correct the errors and try again.", error1)
         self.assertEqual("The credentials provided are incorrect", error2)
 
     def test_07_empty_email_and_password(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys("")
-        driver.find_element(By.ID, "Password").send_keys("")
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        error1 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > span:nth-child(1)").text
-        error2 = driver.find_element(By.CSS_SELECTOR, ".validation-summary-errors > ul:nth-child(2) > li:nth-child(1)").text
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys("")
+        driver.find_element(By.ID, LoginPage.password).send_keys("")
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        error1 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary).text
+        error2 = driver.find_element(By.CSS_SELECTOR, LoginPage.error_summary_detail).text
         self.assertEqual("Login was unsuccessful. Please correct the errors and try again.", error1)
         self.assertEqual("No customer account found", error2)
 
     def test_08_activated_remember_me(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys(DataLogin.email)
-        driver.find_element(By.ID, "Password").send_keys(DataLogin.password)
-        driver.find_element(By.ID, "RememberMe").click()
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        self.assertEqual(driver.current_url, "https://demowebshop.tricentis.com/")
-        data = driver.find_element(By.CSS_SELECTOR, ".header-links > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)").text
-        self.assertEqual("oviliach123@gmail.com", data)
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys(DataLogin.email)
+        driver.find_element(By.ID, LoginPage.password).send_keys(DataLogin.password)
+        driver.find_element(By.ID, LoginPage.rememberme).click()
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        self.assertEqual(driver.current_url, DataLogin.baseURL)
+        data = driver.find_element(By.CSS_SELECTOR, LoginPage.profile_info).text
+        self.assertEqual(DataLogin.email, data)
 
     def test_09_unactivated_remember_me(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.ID, "Email").send_keys(DataLogin.email)
-        driver.find_element(By.ID, "Password").send_keys(DataLogin.password)
-        driver.find_element(By.CSS_SELECTOR, ".button-1.login-button[type=submit]").click()
-        self.assertEqual(driver.current_url, "https://demowebshop.tricentis.com/")
-        data = driver.find_element(By.CSS_SELECTOR, ".header-links > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)").text
-        self.assertEqual("oviliach123@gmail.com", data)
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.ID, LoginPage.email).send_keys(DataLogin.email)
+        driver.find_element(By.ID, LoginPage.password).send_keys(DataLogin.password)
+        driver.find_element(By.CSS_SELECTOR, LoginPage.login_button).click()
+        self.assertEqual(driver.current_url, DataLogin.baseURL)
+        data = driver.find_element(By.CSS_SELECTOR, LoginPage.profile_info).text
+        self.assertEqual(DataLogin.email, data)
     
     def test_10_forgot_password(self):
         driver = self.browser
-        driver.get("https://demowebshop.tricentis.com/login")
-        driver.find_element(By.CSS_SELECTOR, ".forgot-password > a:nth-child(1)").click()
-        self.assertEqual(driver.current_url, "https://demowebshop.tricentis.com/passwordrecovery")
-        data = driver.find_element(By.CSS_SELECTOR, ".page-title > h1:nth-child(1)").text
+        driver.get(f"{DataLogin.baseURL}login")
+        driver.find_element(By.CSS_SELECTOR, LoginPage.forgot_password).click()
+        self.assertEqual(driver.current_url, f"{DataLogin.baseURL}passwordrecovery")
+        data = driver.find_element(By.CSS_SELECTOR, RecoveryPage.header).text
         self.assertEqual("Password recovery", data)
-        driver.find_element(By.ID, "Email").send_keys(DataLogin.email)
-        driver.find_element(By.CSS_SELECTOR, ".password-recovery-button[type=submit]").click()
-        data = driver.find_element(By.CSS_SELECTOR, ".result").text
+        driver.find_element(By.ID, RecoveryPage.email).send_keys(DataLogin.email)
+        driver.find_element(By.CSS_SELECTOR, RecoveryPage.recover_button).click()
+        data = driver.find_element(By.CSS_SELECTOR, RecoveryPage.result).text
         self.assertEqual("Email with instructions has been sent to you.", data)
 
 
